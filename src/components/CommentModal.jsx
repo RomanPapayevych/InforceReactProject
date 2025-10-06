64b6e45bf555a56010d041db92e1b2d1d573caae
Roman <romanpapayevych@gmail.com>
2025-10-06 15:34:57 +0300

    feat: add button and function to add comment

---
+++ src/components/CommentModal.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+const CommentModal = ({onClose, onSave}) => {
+    
+}
+
+export default CommentModal;
--- src/pages/ProductView.jsx
+++ src/pages/ProductView.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import CommentList from "../components/CommentList";
+import CommentModal from "../components/CommentModal";
 
 const ProductView = () => {
     const { id } = useParams();
     const [product, setProduct] = useState(null);
+    const [showCommentModal, setShowCommentModal] = useState(false);
 
     useEffect(() => {
         fetch(`http://localhost:5000/products`)
@@ -17,7 +19,19 @@ const ProductView = () => {
             });
     }, [id]);
 
-  if (!product) return <p>Loading...</p>;
+    const handleAddComment = (comment) => {
+        const updated = {
+            ...product, 
+            comments: [...product.comments, comment]
+        }
+        fetch(`http://localhost:5000/products/${id}`, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(updated),
+        }).then(() => setProduct(updated));
+    }
+
+    if (!product) return <p>Loading...</p>;
   
   return(
     <div style={{ padding: "20px" }}>
@@ -26,7 +40,15 @@ const ProductView = () => {
         <p>Weight: {product.weight}</p>
         <p>Count: {product.count}</p>
 
+        <button onClick={() => setShowCommentModal(true)}>Add Comment</button>
         <CommentList comments={product.comments} />
+
+        {showCommentModal && (
+            <CommentModal
+                onClose={() => setShowCommentModal(false)}
+                onSave={handleAddComment}
+            />
+        )}
     </div>
   )
 }
