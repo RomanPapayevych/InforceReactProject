e482f41995cac80939387b9cf74d4f0ba826c1d7
Roman <romanpapayevych@gmail.com>
2025-10-06 14:19:30 +0300

    feat: add rendering of objects in ProductListView

--- src/pages/ProductsListView.jsx
+++ src/pages/ProductsListView.jsx
@@ -1,6 +1,32 @@
+import { useEffect, useState } from "react";
+import ProductItem from '../components/ProductItem'
 
 const ProductListView = () => {
+    const [products, setProducts] = useState([]);
 
+    useEffect(() => {
+        fetch("http://localhost:5000/products")
+            .then((res) => res.json())
+            .then((data) => setProducts(data));
+    }, []);
+
+    const sortedProducts = [...products].sort((a, b) => {
+        if (sortType === "name") return a.name.localeCompare(b.name);
+        if (sortType === "count") return b.count - a.count;
+        return 0;
+    });
+
+    return(
+        <div>
+            <h1>Products List View</h1>
+            {sortedProducts.map((p) => (
+                <ProductItem
+                    key={p.id}
+                    product={p}
+                />
+            ))}
+        </div>
+    )
 }
 
 export default ProductListView;
